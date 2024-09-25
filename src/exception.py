255d21484ef294707204563d15a27ef2053222ce
Sabbha20 <sabbha.power1@gmail.com>
2024-09-25 16:32:38 +0530

    Project structure and logging & exception

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+import logger
+
+def err_msg_details(err, err_detail:sys):
+    _, _, exc_tb = err_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    err_msg = f"Error occurred: Script[{file_name}] | Line[{exc_tb.tb_lineno}] | Error Message:[{str(err)}]"
+    
+    return err_msg
+
+class MLCustomException(Exception):
+    def __init__(self, err_msg, err_detail:sys):
+        super().__init__(err_msg)
+        self.err_msg = err_msg_details(err_msg, err_detail)
+        
+    def __str__(self):
+        return self.err_msg
+    
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logger.logging.error(MLCustomException(e, sys))
+        raise MLCustomException(e, sys)
---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%d%m%Y_%H%M%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs",datetime.now().strftime('%d-%b-%Y'))
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] | %(name)s - %(lineno)d | [%(levelname)s]: %(message)s",
+)
+
+if __name__ == "__main__":
+    logging.info("Starting the script")
